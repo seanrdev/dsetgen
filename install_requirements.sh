052bdf7e2632d10beed0dea542b639253df150e1
seanrdev <seanrdev@gmail.com>
2026-03-06 14:01:51 -0600

    Update install_requirements.sh
    Added tesseract-ocr to be installed.

--- install_requirements.sh
+++ install_requirements.sh
@@ -2,7 +2,7 @@
 
 # 1. Install the system dependencies required to compile psycopg2 (for pgcli)
 sudo apt update
-sudo apt install -y libpq-dev python3-dev build-essential
+sudo apt install -y libpq-dev python3-dev build-essential tesseract-ocr
 
 # 2. Install the top-level applications using pipx
 pipx install "mitmproxy==12.2.1"
